ea75d572516ae02e72d4238b7359062150ba8f8e
msngl0234 <msngl@icloud.com>
2024-09-08 22:49:16 +0300

    rk

---
+++ c00/ex04/main.c
@@ -0,0 +1,8 @@
+void	ft_is_negative(int n);
+
+int main()
+{
+	int x = -5;
+	ft_is_negative(x);
+	return 0;
+}
---
+++ c00/ex07/main.c
@@ -0,0 +1,7 @@
+void	ft_putnbr(int nb);
+
+int main()
+{
+	ft_putnbr(5);
+	return 0;
+}
